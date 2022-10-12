16f69e840c6028a5de09aa1fc3f96858989b25ef
Brattlof <57799495+Brattlof@users.noreply.github.com>
2022-10-12 15:00:13 +0200

    Update main.cpp

--- Examples/Client/main.cpp
+++ Examples/Client/main.cpp
@@ -8,12 +8,6 @@ int main(void)
 
 	External& rExternal = External::GetInstance();
 
-	if (rExternal.SetProcess("notepad.exe"))
-	{
-		DWORD temp = rExternal.FindProcess("notepad.exe").th32ParentProcessID;
-		std::cout << temp;
-	}
-
 	External::Deinit();
 
 	return 0x0;
